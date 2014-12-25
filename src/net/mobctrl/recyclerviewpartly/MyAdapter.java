87be13e1fb969791529d34cbc4d3c1e174985dfa
郑海波 <hbzheng@v5.cn>
2014-12-25 11:51:47 +0800

    add demo

--- src/net/mobctrl/recyclerviewpartly/MyAdapter.java
+++ src/net/mobctrl/recyclerviewpartly/MyAdapter.java
@@ -1,9 +1,7 @@
 package net.mobctrl.recyclerviewpartly;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import net.mobctrl.recyclerviewpartlyrefreash.R;
 import android.os.Handler;
